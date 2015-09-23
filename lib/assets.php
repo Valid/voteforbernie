3db25895228d653b36b58d884527822fd00fd7e9
Atticus White <contact@atticuswhite.com>
2015-09-23 18:00:04 -0400

    js

--- lib/assets.php
+++ lib/assets.php
@@ -3,7 +3,7 @@ namespace VoteForBernie\Wordpress\Assets;
 
 function assets() {
   wp_enqueue_script('vfb_vendor_js', get_template_directory_uri() . '/dist/vendor.js', array('jquery'), null, true);
-  wp_enqueue_script('vfb_site_js', get_template_directory_uri() . '/dist/site.js', array('vfb_vendor'), null, true);
+  wp_enqueue_script('vfb_site_js', get_template_directory_uri() . '/dist/site.js', array('vfb_vendor_js'), null, true);
   wp_enqueue_style('vfb_vendor_css', get_template_directory_uri() . '/dist/vendor.css');
   wp_enqueue_style('vfb_site_css', get_template_directory_uri() . '/dist/style.css');
 }
