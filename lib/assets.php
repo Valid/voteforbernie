efb2e5b41c56fb7d651fdf6a39a3a49fbada0e74
Jon Hughes <github@validatorian.com>
2015-09-28 12:07:43 -0700

    Change load order of scripts

--- lib/assets.php
+++ lib/assets.php
@@ -8,8 +8,8 @@ function assets() {
   $siteCss = WP_LOCAL_SERVERS ? 'style.css' : 'style.min.css';
 
   wp_enqueue_script('vfb_vendor_js', get_template_directory_uri() . "/dist/$vendorJs", array('jquery'), null, true);
-  wp_enqueue_script('vfb_site_js', get_template_directory_uri() . "/dist/$siteJs", array('vfb_vendor_js'), null, true);
   wp_enqueue_style('vfb_vendor_css', get_template_directory_uri() . "/dist/$vendorCss");
+  wp_enqueue_script('vfb_site_js', get_template_directory_uri() . "/dist/$siteJs", array('vfb_vendor_js'), null, true);
   wp_enqueue_style('vfb_site_css', get_template_directory_uri() . "/dist/$siteCss");
 }
 add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\assets', 100);
