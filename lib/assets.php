ec6313b01abc22d9103a8d9cc0836b07c83d0955
Jon Hughes <github@validatorian.com>
2015-10-02 16:25:51 -0700

    Fix typo

--- lib/assets.php
+++ lib/assets.php
@@ -5,7 +5,7 @@ function assets() {
   $vendorJs = WP_LOCAL_SERVER ? 'vendor.js' : 'vendor.min.js';
   $siteJs = WP_LOCAL_SERVER ? 'site.js' : 'site.min.js';
   $vendorCss = WP_LOCAL_SERVER ? 'vendor.css' : 'vendor.min.css';
-  $siteCss = WP_LOCAL_SERVERS ? 'style.css' : 'style.min.css';
+  $siteCss = WP_LOCAL_SERVER ? 'style.css' : 'style.min.css';
 
   wp_enqueue_script('vfb_vendor_js', get_template_directory_uri() . "/dist/$vendorJs", array('jquery'), null, true);
   wp_enqueue_style('vfb_vendor_css', get_template_directory_uri() . "/dist/$vendorCss");
