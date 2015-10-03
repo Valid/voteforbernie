bd9f7b0fff04f321c2b068b2cc84e8a3a9779078
Jon Hughes <github@validatorian.com>
2015-10-02 23:49:17 -0700

    Fixing spaces

--- header.php
+++ header.php
@@ -5,87 +5,87 @@
 <!--[if (IE 8)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9"><![endif]-->
 <!--[if gt IE 8]><!--> <html <?php language_attributes(); ?> class="no-js"><!--<![endif]-->
 
-  <head>
-    <meta charset="utf-8">
+<head>
+  <meta charset="utf-8">
 
-    <?php // force Internet Explorer to use the latest rendering engine available ?>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <?php // force Internet Explorer to use the latest rendering engine available ?>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-    <title><?php wp_title(''); ?></title>
+  <title><?php wp_title(''); ?></title>
 
-    <?php // mobile meta (hooray!) ?>
-    <meta name="HandheldFriendly" content="True">
-    <meta name="MobileOptimized" content="320">
-    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+  <?php // mobile meta (hooray!) ?>
+  <meta name="HandheldFriendly" content="True">
+  <meta name="MobileOptimized" content="320">
+  <meta name="viewport" content="width=device-width, initial-scale=1"/>
 
-    <?php // icons & favicons (for more: http://www.jonathantneal.com/blog/understand-the-favicon/) ?>
-    <link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/dist/images/apple-touch-icon.png">
-    <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.png">
-    <link rel="apple-touch-icon" sizes="57x57" href="<?php echo get_template_directory_uri(); ?>/dist/images/apple-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="<?php echo get_template_directory_uri(); ?>/dist/images/apple-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/dist/images/apple-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_template_directory_uri(); ?>/dist/images/apple-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/dist/images/apple-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="<?php echo get_template_directory_uri(); ?>/dist/images/apple-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_template_directory_uri(); ?>/dist/images/apple-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="<?php echo get_template_directory_uri(); ?>/dist/images/apple-icon-152x152.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/dist/images/apple-icon-180x180.png">
-    <link rel="icon" type="image/png" sizes="192x192"  href="<?php echo get_template_directory_uri(); ?>/dist/images/android-icon-192x192.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicon-96x96.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicon-16x16.png">
+  <?php // icons & favicons (for more: http://www.jonathantneal.com/blog/understand-the-favicon/) ?>
+  <link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/dist/images/apple-touch-icon.png">
+  <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.png">
+  <link rel="apple-touch-icon" sizes="57x57" href="<?php echo get_template_directory_uri(); ?>/dist/images/apple-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="<?php echo get_template_directory_uri(); ?>/dist/images/apple-icon-60x60.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/dist/images/apple-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_template_directory_uri(); ?>/dist/images/apple-icon-76x76.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/dist/images/apple-icon-114x114.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="<?php echo get_template_directory_uri(); ?>/dist/images/apple-icon-120x120.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_template_directory_uri(); ?>/dist/images/apple-icon-144x144.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="<?php echo get_template_directory_uri(); ?>/dist/images/apple-icon-152x152.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/dist/images/apple-icon-180x180.png">
+  <link rel="icon" type="image/png" sizes="192x192"  href="<?php echo get_template_directory_uri(); ?>/dist/images/android-icon-192x192.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="96x96" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicon-96x96.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicon-16x16.png">
 
-    <link rel="manifest" href="/manifest.json">
+  <link rel="manifest" href="/manifest.json">
     <!--[if IE]>
       <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
-    <![endif]-->
-    <?php // or, set /favicon.ico for IE10 win ?>
-    <meta name="msapplication-TileColor" content="#323944">
-    <meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/dist/images/win8-tile-icon.png">
-    <meta name="theme-color" content="#323944">
+      <![endif]-->
+      <?php // or, set /favicon.ico for IE10 win ?>
+      <meta name="msapplication-TileColor" content="#323944">
+      <meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/dist/images/win8-tile-icon.png">
+      <meta name="theme-color" content="#323944">
 
-    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+      <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 
-    <!-- Optimizely A/B testing snippet -->
-    <script src="//cdn.optimizely.com/js/3517780176.js"></script>
+      <!-- Optimizely A/B testing snippet -->
+      <script src="//cdn.optimizely.com/js/3517780176.js"></script>
 
-    <?php // wordpress head functions ?>
-    <?php wp_head(); ?>
-    <?php // end of wordpress head ?>
+      <?php // wordpress head functions ?>
+      <?php wp_head(); ?>
+      <?php // end of wordpress head ?>
 
-    <?php // drop Google Analytics Here ?>
-    <script>
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+      <?php // drop Google Analytics Here ?>
+      <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-      ga('create', 'UA-64072805-1', 'auto');
-      ga('send', 'pageview');
+        ga('create', 'UA-64072805-1', 'auto');
+        ga('send', 'pageview');
 
-    </script>
+      </script>
 
-    <script>
-      window.fbAsyncInit = function() {
-        FB.init({
-          appId      : '1615439018737096',
-          xfbml      : true,
-          version    : 'v2.4'
-        });
-      };
+      <script>
+        window.fbAsyncInit = function() {
+          FB.init({
+            appId      : '1615439018737096',
+            xfbml      : true,
+            version    : 'v2.4'
+          });
+        };
 
-      (function(d, s, id){
+        (function(d, s, id){
          var js, fjs = d.getElementsByTagName(s)[0];
          if (d.getElementById(id)) {return;}
          js = d.createElement(s); js.id = id;
          js.src = "//connect.facebook.net/en_US/sdk.js";
          fjs.parentNode.insertBefore(js, fjs);
        }(document, 'script', 'facebook-jssdk'));
-    </script>
+     </script>
 
-    <script>window.twttr = (function(d, s, id) {
+     <script>window.twttr = (function(d, s, id) {
       var js, fjs = d.getElementsByTagName(s)[0],
-        t = window.twttr || {};
+      t = window.twttr || {};
       if (d.getElementById(id)) return t;
       js = d.createElement(s);
       js.id = id;
@@ -126,11 +126,11 @@
 
          <div id="inner-header" class="branding wrap cf">
 
-            <?php // to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> ?>
-            <h1 id="logo" class="h1" itemscope itemtype="http://schema.org/Organization"><a href="<?php echo home_url(); ?>" rel="nofollow">#VoteForBernie</a></h1>
-            <nav role="navigation" itemscope itemtype="http://schema.org/SiteNavigationElement">
-              <?php wp_nav_menu(array(
-                'theme_location' => 'header'
+          <?php // to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> ?>
+          <h1 id="logo" class="h1" itemscope itemtype="http://schema.org/Organization"><a href="<?php echo home_url(); ?>" rel="nofollow">#VoteForBernie</a></h1>
+          <nav role="navigation" itemscope itemtype="http://schema.org/SiteNavigationElement">
+            <?php wp_nav_menu(array(
+              'theme_location' => 'header'
               )); ?>
 
             </nav>
