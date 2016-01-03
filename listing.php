67629881ce201e0feef1e7e71a18035c23fdbf1b
Jon Hughes <github@validatorian.com>
2016-01-02 16:10:15 -0800

    Add more data to state listing for verifying on staging

--- listing.php
+++ listing.php
@@ -34,6 +34,7 @@ $mostRecentStateUpdate = $stateService->determineMostRecentUpdate($states);
                     <div class="states">
                     <?php // if(function_exists('add_social_button_in_content')) echo add_social_button_in_content(); ?>
 
+                <h3>Basic Information</h3>
                 <table>
                   <tr>
                     <th>State</th>
@@ -43,34 +44,59 @@ $mostRecentStateUpdate = $stateService->determineMostRecentUpdate($states);
                     <th>Primary/Caucus Date</th>
                     <th>Registration Deadline Date</th>
                     <th>Affiliation Deadline Date</th>
-                    <!-- <th>Early Voting Begins</th>
-                    <th>Early Voting Ends</th>
-                    <th>Absentee Ballot Request Deadline</th>
-                    <th>Absentee Ballot Postmark Deadling</th>
-                    <th>Absentee Excuse Required</th>
-                    <th>Overseas Application Deadline</th>
-                    <th>Overseas Postmark Deadline</th> -->
+                    <th>Vote in primaries when 17?</th>
                   </tr>
 
                   <?php foreach ($states as $state): ?>
 
 
                     <tr>
-                      <!-- <td><a href="<?php echo home_url(); ?>/wp-admin/post.php?post=<?php echo $state->post->ID; ?>&amp;action=edit"><?php echo $state->getTitle(); ?></a></td> -->
-                      <td><?php echo $state->getTitle(); ?></td>
+                      <?php if (current_user_can( 'manage_options' )) { ?>
+                        <td><a href="<?php echo home_url(); ?>/wp-admin/post.php?post=<?php echo $state->post->ID; ?>&amp;action=edit"><?php echo $state->getTitle(); ?></a></td>
+                      <?php } else { ?>
+                        <td><?php echo $state->getTitle(); ?></td>
+                      <?php } ?>
                       <td><?php echo $state->state_phone; ?></td>
                       <td><?php echo $state->type; ?></td>
                       <td><?php echo $state->status; ?></td>
                       <td><?php echo $state->primary_date ? date('F j, Y', strtotime($state->primary_date)) : ''; ?></td>
                       <td><?php echo $state->deadline_date ? date('F j, Y', strtotime($state->deadline_date)) : ''; ?></td>
                       <td><?php echo $state->aff_deadline_date ? date('F j, Y', strtotime($state->aff_deadline_date)) : ''; ?></td>
-                      <!-- <td><?php echo $helper->formatDate($state->early_voting_start); ?></td>
+                      <td><?php echo ($state->under_18 ? 'yes' : 'no'); ?></td>
+                    </tr>
+                  <?php endforeach; ?>
+                </table>
+
+                <h3>Extended Information</h3>
+                <table>
+                  <tr>
+                    <th>State</th>
+                    <th>Check Registration</th>
+                    <th>Register Online</th>
+                    <th>Early Voting Begins</th>
+                    <th>Early Voting Ends</th>
+                    <th>Absentee Ballot Request Deadline</th>
+                    <th>Absentee Ballot Postmark Deadling</th>
+                    <th>Absentee Excuse Required</th>
+                    <th>Overseas Application Deadline</th>
+                    <th>Overseas Postmark Deadline</th>
+                  </tr>
+                  <?php foreach ($states as $state): ?>
+                    <tr>
+                      <?php if (current_user_can( 'manage_options' )) { ?>
+                        <td><a href="<?php echo home_url(); ?>/wp-admin/post.php?post=<?php echo $state->post->ID; ?>&amp;action=edit"><?php echo $state->getTitle(); ?></a></td>
+                      <?php } else { ?>
+                        <td><?php echo $state->getTitle(); ?></td>
+                      <?php } ?>
+                      <td><a href="<?php echo $state->check_registration_link ?>" data-track="ChkLnk,<?php echo $state->state; ?>" target="_blank">Check Registration Online</a></td>
+                      <td><a href="<?php echo $helper->getOnlineRegistrationLink($state); ?>" data-track="regBtn,<?php echo $state->state; ?>">Register Online</a></td>
+                      <td><?php echo $helper->formatDate($state->early_voting_start); ?></td>
                       <td><?php echo $helper->formatDate($state->early_voting_end); ?></td>
                       <td><?php echo $helper->formatDate($state->absentee_app_deadline); ?></td>
                       <td><?php echo $helper->formatDate($state->absentee_postmark_deadline); ?></td>
                       <td><?php if ($state->absenteeExcuseRequired()) { ?>yes<?php } else {?>no<?php } ?></td>
                       <td><?php echo $helper->formatDate($state->overseas_app_deadline); ?></td>
-                      <td><?php echo $helper->formatDate($state->overseas_postmark_deadline); ?></td> -->
+                      <td><?php echo $helper->formatDate($state->overseas_postmark_deadline); ?></td>
                     </tr>
                   <?php endforeach; ?>
                 </table>
