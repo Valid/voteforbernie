5085a20e8e8f0c72c1780e1e4083dda860a722c0
Jon Hughes <github@validatorian.com>
2016-01-16 13:48:49 -0800

    Remove unnecessary function

--- assets/js/site.js
+++ assets/js/site.js
@@ -90,23 +90,6 @@ var timeToWaitForLast = 100;
  *
 */
 
-/*
- * We're going to swap out the gravatars.
- * In the functions.php file, you can see we're not loading the gravatar
- * images on mobile to save bandwidth. Once we hit an acceptable viewport
- * then we can swap out those images since they are located in a data attribute.
-*/
-function loadGravatars() {
-  // set the viewport using the function above
-  viewport = updateViewportDimensions();
-  // if the viewport is tablet or larger, we load in the gravatars
-  if (viewport.width >= 768) {
-  jQuery('.comment img[data-gravatar]').each(function(){
-    jQuery(this).attr('src',jQuery(this).attr('data-gravatar'));
-  });
-	}
-} // end function
-
 var activeCallback = jQuery.Callbacks();
 
 vfb.loadFonts = function () {
@@ -490,8 +473,6 @@ vfb.handleNewsletters = function () {
  * Put all your regular jQuery in here.
 */
 jQuery(document).ready(function($) {
-
-  loadGravatars();
 
   vfb.handleNewsletters();
 
