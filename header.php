27cb1720936ed68e4eb34184874836580e84edaf
Jon Hughes <github@validatorian.com>
2016-01-17 19:45:09 -0800

    Change actionbar to post debate

--- header.php
+++ header.php
@@ -114,7 +114,8 @@
         <header class="header" role="banner" itemscope itemtype="http://schema.org/WPHeader">
           <div class="action-bar np">
             <?php // TODO: Pull from ACF ?>
-            <strong>VoteForBernie Facelift &mdash;</strong> New look, new content, same goal: <strong>Get Out The Vote!</strong> <a href="/alerts/voteforbernie-two-point-oh">See What's New</a>
+            <?php // <strong>VoteForBernie Facelift &mdash;</strong> New look, new content, same goal: <strong>Get Out The Vote!</strong> <a href="/alerts/voteforbernie-two-point-oh">See What's New</a> ?>
+            <strong>Democratic Debate &mdash;</strong> Feeling the Bern after watching the debate? <a href="https://secure.berniesanders.com/page/outreach/view/grassroots-fundraising/VFB" data-track="ActionBar,debateDonate">Donate to Bernie!</a>
             <div class="fb-like" data-href="http://voteforbernie.org/" data-layout="button_count" data-action="like" data-show-faces="true" data-share="false"></div>
           </div>
           <div id="inner-header" class="branding wrap">
