a80b9fc45d1cbed1da88851078d6bef46541605d
Jon Hughes <github@validatorian.com>
2016-01-26 15:10:25 -0800

    Switch link back to VFB fundraiser

--- functions.php
+++ functions.php
@@ -4,6 +4,7 @@
 $vfb_includes = array(
   'lib/init.php',
   'lib/assets.php',
+  // 'lib/shortcodes.php',
   'lib/models/post-model.php',
   'lib/models/state-model.php',
   'lib/services/state-service.php',
@@ -16,6 +17,36 @@ foreach ($vfb_includes as $file) {
     trigger_error(sprtinf('Error locating %s for inclusion', $file), E_USER_ERROR);
   }
   require_once($filepath);
+}
+
+if( function_exists('acf_add_options_page') ) {
+
+  acf_add_options_page(array(
+    'page_title'  => 'Action Bar'
+  ));
+
+}
+
+function daysAway($date) {
+  $dateObj = strtotime($date);
+  $diff = $dateObj - strtotime('now');
+  $daysAway = floor($diff/60/60/24 + 1);
+
+  $s = ($daysAway != 1 ? 's' : '');
+
+  if ($daysAway == 0) {
+    $fDate = 'today!';
+  } else {
+    $fDate = $daysAway . ' day' . $s;
+
+    if ($diff < 0) {
+      $fDate .= ' ago';
+    } else {
+      $fDate .= ' to go';
+    }
+  }
+
+  return $fDate;
 }
 
 // cleanup global vars
