823cfa61921c248245f45ebb9e7a3a1a69dbb90f
Jon Hughes <github@validatorian.com>
2016-02-29 04:12:49 -0800

    Add GOTV page

---
+++ gotv.php
@@ -0,0 +1,109 @@
+<?php
+  /* Template Name: GOTV */
+
+  use VoteForBernie\Wordpress\Services\StateService;
+  use VoteForBernie\Wordpress\Helpers\VoteInfoHelper;
+  $stateService = new StateService();
+  $helper = new VoteInfoHelper();
+  $states = $stateService->getStates(); ?>
+
+<?php get_header(); ?>
+
+<div class="gotv-page">
+
+  <main id="main" class="m-all t-all d-all cf" role="main">
+
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+    <article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+
+      <div class="map-intro">
+        <h1 class="page-title">#<strong>Bern</strong>TheBallot</h1>
+        <div class="state-select">
+          <p>Bernie's campaign is unique. He doesn't have a Super PAC.</p>
+          <p>He is relying on us, the young and working-class citizens of America to do something radical:</p>
+          <h3>Get Out The Vote!</h3>
+          <!--
+          <p>What do to? <span class="no-mobile">your state by clicking it or</span>
+            <select class="state-selector">
+              <option>Select Your State</option>
+              <?php foreach ($states as $state): ?>
+                <option value="<?php echo $state->state; ?>"><?php echo $state->getTitle(); ?></option>
+              <?php endforeach; ?>
+            </select>
+          </p> -->
+          <!-- <p class="map-link np"><a href="<?php echo home_url(); ?>">Not registered to vote? <span>Find your state!</span></a></p> -->
+        </div>
+
+      </div>
+
+      <section class="entry-content cf wrap" itemprop="articleBody">
+        <h3 class="page-title"><strong>What</strong> to do</h3>
+
+
+          <p>We need <strong>every able person</strong> to be helping get voter turnout up in Super Tuesday states. If you haven't volunteered yet, this is your chance. Bernie is fighting an uphill battle and needs the support of the grassroots community, and that means you.</p>
+          <p>Super Tuesday states: <strong>Alabama, Arkansas, Colorado, Georgia, Kansas, Maine, Massachusetts, Michigan  Minnesota, Nebraska, Oklahoma, Tennessee, Texas, and Virginia.</strong></p>
+          <p><strong>Democracy is not a spectator sport.</strong></p>
+
+          <?php if(function_exists('add_social_button_in_content')) echo add_social_button_in_content(); ?>
+        <div class="content canvass">
+          <h3>Canvas for Bernie</h3>
+          <p>Tried and true. knocking on doors has been the most effective method of Getting Out The Vote for a long time running. This is what everyone able <strong>should</strong> be doing. If you've never canvassed before, you're certainly not alone! You can receive training and instructions on where and how to canvass from a Bernie Sanders campaign office.</p>
+          <p>In a Super Tuesday state and willing to knock on doors? <a href="http://map.berniesanders.com/go-the-distance/" class="ui-btn cta" data-track="GOTV,Canvas">Find your local office for Bernie</a></p>
+          <p>Near a Super Tuesday state and willing to cross state lines? <a href="https://go.berniesanders.com/page/s/go-the-distance-for-bernie" class="ui-btn cta" data-track="GOTV,Canvas">Canvas for Bernie Out-Of-State</a></p>
+        </div>
+        <div class="content alt phonebank">
+          <h3>Phonebank for Bernie</h3>
+          <p>If you cannot canvas, the next best thing is phonebanking. All you have to do is call voters and let them know when and where they can vote. Training is provided!</p>
+          <p><a href="https://go.berniesanders.com/page/content/phonebank" class="ui-btn cta" data-track="GOTV,Phonebank">Phonebank for Bernie</a></p>
+        </div>
+        <div class="content facebank">
+          <h3>"Facebank" for Bernie</h3>
+          <p>This is new... Facebook allows you to create very specific searches. In fact, you can search for everyone that has 'liked' Bernie Sanders (likely Bernie supporter) AND is your friend or friend of a friend AND is from a specific state. In this way, you can contact people who are Bernie supporters and remind them to vote!</p>
+          <p><a href="http://berniefriendfinder.com/" class="ui-btn cta" data-track="GOTV,BernieFriendFinder">Facebank for Bernie</a></p>
+        </div>
+
+        <div class="content alt donate">
+          <h3>Donate to Bernie</h3>
+          <p>If you're unable to volunteer, consider giving a donation to Bernie's campaign. Bernie is the only Democratic candidate not using a SuperPac and relies on donations from us to support his campaign.</p>
+          <p><a href="http://berniesanders.com/reddit" class="ui-btn cta" data-track="GOTV,Donate">Donate to Bernie's Campaign</a></p>
+        </div>
+        <div class="content stay-informed">
+          <h3>Stay informed</h3>
+          <p>The primary season is well underway. Sign up to be reminded of deadlines in your state and other important information.</p>
+          <?php echo yksemeProcessSnippet( "2da18e85f7" , "Keep me informed!" ); ?>
+          <p>Did you know there is a community of over 100,000 Bernie supporters online? Join us!</p>
+          <p><a href="http://SandersForPresident.reddit.com" class="ui-btn cta" data-track="GOTV,Reddit">Join the Community</a></p>
+        </div>
+
+        <?php if(function_exists('add_social_button_in_content')) echo add_social_button_in_content(); ?>
+        <h2 class="page-title">#WeStand<strong>Together</strong></h2>
+      </section>
+
+    </article>
+
+    <?php endwhile; else : ?>
+
+    <article id="post-not-found" class="hentry cf">
+      <header class="article-header">
+        <h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
+      </header>
+      <section class="entry-content">
+        <p>
+          <?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?>
+        </p>
+      </section>
+      <footer class="article-footer">
+        <p>
+          <?php _e( 'This is the error message in the page-custom.php template.', 'bonestheme' ); ?>
+        </p>
+      </footer>
+    </article>
+
+    <?php endif; ?>
+
+  </main>
+
+</div>
+
+<?php get_footer(); ?>
--- listing.php
+++ listing.php
@@ -1,157 +1,183 @@
 <?php
-/*
- Template Name: Deadline Information By State Page
- *
-*/
+  /* Template Name: GOTV **/
 
-use VoteForBernie\Wordpress\Services\StateService;
-use VoteForBernie\Wordpress\Helpers\VoteInfoHelper;
-$stateService = new StateService();
-$helper = new VoteInfoHelper();
-$states = $stateService->getStates();
-$mostRecentStateUpdate = $stateService->determineMostRecentUpdate($states);
-?>
+  use VoteForBernie\Wordpress\Services\StateService;
+  use VoteForBernie\Wordpress\Helpers\VoteInfoHelper;
+  $stateService = new StateService();
+  $helper = new VoteInfoHelper();
+  $states = $stateService->getStates(); ?>
 
 <?php get_header(); ?>
 
-      <div class="vote-info">
+<div class="gotv-page">
+
+  <main id="main" class="m-all t-all d-all cf" role="main">
+
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+    <article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+
+      <header class="article-header">
+        <h1 class="page-title"><?php the_title(); ?></h1>
+      </header>
+
+      <section class="entry-content cf" itemprop="articleBody">
+        <div class="page-content">
+          <div class="states">
+            <?php // if(function_exists( 'add_social_button_in_content')) echo add_social_button_in_content(); ?>
+
+            <h3>Basic Information</h3>
+            <table>
+              <tr>
+                <th>State</th>
+                <th>Phone #</th>
+                <th>Type</th>
+                <th>Status</th>
+                <th>Primary/Caucus Date</th>
+                <th>Registration Deadline Date</th>
+                <th>Affiliation Deadline Date</th>
+                <th>Vote in primaries when 17?</th>
+              </tr>
+
+              <?php foreach ($states as $state): ?>
 
 
-        <div>
+              <tr>
+                <?php if (current_user_can( 'manage_options' )) { ?>
+                <td>
+                  <a href="<?php echo home_url(); ?>/wp-admin/post.php?post=<?php echo $state->post->ID; ?>&amp;action=edit">
+                    <?php echo $state->getTitle(); ?></a>
+                </td>
+                <?php } else { ?>
+                <td>
+                  <?php echo $state->getTitle(); ?></td>
+                <?php } ?>
+                <td>
+                  <?php echo $state->state_phone; ?></td>
+                <td>
+                  <?php echo $state->type; ?></td>
+                <td>
+                  <?php echo $state->status; ?></td>
+                <td>
+                  <?php echo $state->primary_date ? date('F j, Y', strtotime($state->primary_date)) : ''; ?></td>
+                <td>
+                  <?php echo $state->deadline_date ? date('F j, Y', strtotime($state->deadline_date)) : ''; ?></td>
+                <td>
+                  <?php echo $state->aff_deadline_date ? date('F j, Y', strtotime($state->aff_deadline_date)) : ''; ?></td>
+                <td>
+                  <?php echo ($state->under_18 ? 'yes' : ''); ?></td>
+              </tr>
+              <?php endforeach; ?>
+            </table>
 
-            <main id="main" class="m-all t-all d-all cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
+            <h3>Extended Information</h3>
+            <table>
+              <tr>
+                <th>State</th>
+                <th>Early Voting Begins</th>
+                <th>Early Voting Ends</th>
+                <th>Absentee Ballot Request Deadline</th>
+                <th>Absentee Ballot Postmark Deadling</th>
+                <th>Absentee Excuse Required</th>
+                <th>Overseas Application Deadline</th>
+                <th>Overseas Postmark Deadline</th>
+              </tr>
+              <?php foreach ($states as $state): ?>
+              <tr>
+                <?php if (current_user_can( 'manage_options' )) { ?>
+                <td>
+                  <a href="<?php echo home_url(); ?>/wp-admin/post.php?post=<?php echo $state->post->ID; ?>&amp;action=edit">
+                    <?php echo $state->getTitle(); ?></a>
+                </td>
+                <?php } else { ?>
+                <td>
+                  <?php echo $state->getTitle(); ?></td>
+                <?php } ?>
+                <td>
+                  <?php echo $helper->formatDate($state->early_voting_start); ?></td>
+                <td>
+                  <?php echo $helper->formatDate($state->early_voting_end); ?></td>
+                <td>
+                  <?php echo $helper->formatDate($state->absentee_app_deadline); ?></td>
+                <td>
+                  <?php echo $helper->formatDate($state->absentee_postmark_deadline); ?></td>
+                <td>
+                  <?php if ($state->absenteeExcuseRequired()) { ?>yes
+                  <?php } else {?>no
+                  <?php } ?>
+                </td>
+                <td>
+                  <?php echo $helper->formatDate($state->overseas_app_deadline); ?></td>
+                <td>
+                  <?php echo $helper->formatDate($state->overseas_postmark_deadline); ?></td>
+              </tr>
+              <?php endforeach; ?>
+            </table>
 
-              <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
-              <article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
-
-                <header class="article-header">
-                  <h1 class="page-title"><?php the_title(); ?></h1>
-                </header>
-
-                <section class="entry-content cf" itemprop="articleBody">
-                  <div class="page-content">
-                    <div class="states">
-                    <?php // if(function_exists('add_social_button_in_content')) echo add_social_button_in_content(); ?>
-
-                <h3>Basic Information</h3>
-                <table>
-                  <tr>
-                    <th>State</th>
-                    <th>Phone #</th>
-                    <th>Type</th>
-                    <th>Status</th>
-                    <th>Primary/Caucus Date</th>
-                    <th>Registration Deadline Date</th>
-                    <th>Affiliation Deadline Date</th>
-                    <th>Vote in primaries when 17?</th>
-                  </tr>
-
-                  <?php foreach ($states as $state): ?>
-
-
-                    <tr>
-                      <?php if (current_user_can( 'manage_options' )) { ?>
-                        <td><a href="<?php echo home_url(); ?>/wp-admin/post.php?post=<?php echo $state->post->ID; ?>&amp;action=edit"><?php echo $state->getTitle(); ?></a></td>
-                      <?php } else { ?>
-                        <td><?php echo $state->getTitle(); ?></td>
-                      <?php } ?>
-                      <td><?php echo $state->state_phone; ?></td>
-                      <td><?php echo $state->type; ?></td>
-                      <td><?php echo $state->status; ?></td>
-                      <td><?php echo $state->primary_date ? date('F j, Y', strtotime($state->primary_date)) : ''; ?></td>
-                      <td><?php echo $state->deadline_date ? date('F j, Y', strtotime($state->deadline_date)) : ''; ?></td>
-                      <td><?php echo $state->aff_deadline_date ? date('F j, Y', strtotime($state->aff_deadline_date)) : ''; ?></td>
-                      <td><?php echo ($state->under_18 ? 'yes' : ''); ?></td>
-                    </tr>
-                  <?php endforeach; ?>
-                </table>
-
-                <h3>Extended Information</h3>
-                <table>
-                  <tr>
-                    <th>State</th>
-                    <th>Early Voting Begins</th>
-                    <th>Early Voting Ends</th>
-                    <th>Absentee Ballot Request Deadline</th>
-                    <th>Absentee Ballot Postmark Deadling</th>
-                    <th>Absentee Excuse Required</th>
-                    <th>Overseas Application Deadline</th>
-                    <th>Overseas Postmark Deadline</th>
-                  </tr>
-                  <?php foreach ($states as $state): ?>
-                    <tr>
-                      <?php if (current_user_can( 'manage_options' )) { ?>
-                        <td><a href="<?php echo home_url(); ?>/wp-admin/post.php?post=<?php echo $state->post->ID; ?>&amp;action=edit"><?php echo $state->getTitle(); ?></a></td>
-                      <?php } else { ?>
-                        <td><?php echo $state->getTitle(); ?></td>
-                      <?php } ?>
-                      <td><?php echo $helper->formatDate($state->early_voting_start); ?></td>
-                      <td><?php echo $helper->formatDate($state->early_voting_end); ?></td>
-                      <td><?php echo $helper->formatDate($state->absentee_app_deadline); ?></td>
-                      <td><?php echo $helper->formatDate($state->absentee_postmark_deadline); ?></td>
-                      <td><?php if ($state->absenteeExcuseRequired()) { ?>yes<?php } else {?>no<?php } ?></td>
-                      <td><?php echo $helper->formatDate($state->overseas_app_deadline); ?></td>
-                      <td><?php echo $helper->formatDate($state->overseas_postmark_deadline); ?></td>
-                    </tr>
-                  <?php endforeach; ?>
-                </table>
-
-                <h3>Online Action Links</h3>
-                <table>
-                  <tr>
-                    <th>State</th>
-                    <th>Has Online Registration?</th>
-                    <th>Check Registration</th>
-                    <th>Register Online</th>
-                  </tr>
-                  <?php foreach ($states as $state): ?>
-                    <tr>
-                      <?php if (current_user_can( 'manage_options' )) { ?>
-                        <td><a href="<?php echo home_url(); ?>/wp-admin/post.php?post=<?php echo $state->post->ID; ?>&amp;action=edit"><?php echo $state->getTitle(); ?></a></td>
-                      <?php } else { ?>
-                        <td><?php echo $state->getTitle(); ?></td>
-                      <?php } ?>
-                      <td><?php echo ($state->online_reg ? 'yes' : ''); ?></td>
-                      <td>
-                      <?php if ($state->check_registration_link) { ?>
-                        <a href="<?php echo $state->check_registration_link ?>" data-track="ChkLnk,<?php echo $state->state; ?>" target="_blank"><?php echo $state->check_registration_link ?></a>
-                      <?php } ?>
-                      </td>
-                      <td>
-                        <?php if ($state->register_link) { ?>
-                          <a href="<?php echo $state->register_link ?>" data-track="regBtn,<?php echo $state->state; ?>"><?php echo $state->register_link ?></a>
-                        <?php } ?>
-                      </td>
-                    </tr>
-                  <?php endforeach; ?>
-                </table>
-              </div>
-             </div>
-             </section>
-
-              </article>
-
-              <?php endwhile; else : ?>
-
-                  <article id="post-not-found" class="hentry cf">
-                      <header class="article-header">
-                        <h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
-                    </header>
-                      <section class="entry-content">
-                        <p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
-                    </section>
-                    <footer class="article-footer">
-                        <p><?php _e( 'This is the error message in the page-custom.php template.', 'bonestheme' ); ?></p>
-                    </footer>
-                  </article>
-
-              <?php endif; ?>
-
-            </main>
-
+            <h3>Online Action Links</h3>
+            <table>
+              <tr>
+                <th>State</th>
+                <th>Has Online Registration?</th>
+                <th>Check Registration</th>
+                <th>Register Online</th>
+              </tr>
+              <?php foreach ($states as $state): ?>
+              <tr>
+                <?php if (current_user_can( 'manage_options' )) { ?>
+                <td>
+                  <a href="<?php echo home_url(); ?>/wp-admin/post.php?post=<?php echo $state->post->ID; ?>&amp;action=edit">
+                    <?php echo $state->getTitle(); ?></a>
+                </td>
+                <?php } else { ?>
+                <td>
+                  <?php echo $state->getTitle(); ?></td>
+                <?php } ?>
+                <td>
+                  <?php echo ($state->online_reg ? 'yes' : ''); ?></td>
+                <td>
+                  <?php if ($state->check_registration_link) { ?>
+                  <a href="<?php echo $state->check_registration_link ?>" data-track="ChkLnk,<?php echo $state->state; ?>" target="_blank">
+                    <?php echo $state->check_registration_link ?></a>
+                  <?php } ?>
+                </td>
+                <td>
+                  <?php if ($state->register_link) { ?>
+                  <a href="<?php echo $state->register_link ?>" data-track="regBtn,<?php echo $state->state; ?>">
+                    <?php echo $state->register_link ?></a>
+                  <?php } ?>
+                </td>
+              </tr>
+              <?php endforeach; ?>
+            </table>
+          </div>
         </div>
+      </section>
 
-      </div>
+    </article>
+
+    <?php endwhile; else : ?>
+
+    <article id="post-not-found" class="hentry cf">
+      <header class="article-header">
+        <h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
+      </header>
+      <section class="entry-content">
+        <p>
+          <?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?>
+        </p>
+      </section>
+      <footer class="article-footer">
+        <p>
+          <?php _e( 'This is the error message in the page-custom.php template.', 'bonestheme' ); ?>
+        </p>
+      </footer>
+    </article>
+
+    <?php endif; ?>
+
+  </main>
+
+</div>
 
 <?php get_footer(); ?>
