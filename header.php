4ea8bcc6af64c9195babc247a88cfab9a9cd03fa
Jon Hughes <github@validatorian.com>
2016-03-17 23:11:10 -0700

    Header to GOTR

--- header.php
+++ header.php
@@ -119,8 +119,8 @@
             <?php // <strong>#DebateWithBernie &mdash;</strong> Feeling the Bern after the debate?  <a href="https://secure.berniesanders.com/page/outreach/view/grassroots-fundraising/VFB" data-track="ActionBar,msnbcDonate">Donate to Bernie!</a> ?>
             <?php // <strong>Don't Miss Out! &mdash;</strong> 19 deadlines passed, get registered today!</strong> <a href="/schedule/" data-track="CTA,schedule">See Primary Schedule</a> ?>
             <?php // <strong>Phone Bank for Bernie &mdash;</strong> South Carolina votes in a week! </strong> <a href="https://go.berniesanders.com/page/content/scphonebank/" data-track="CTA,phonebank">Call Voters in SC</a> ?>
-            <?php // <strong>New Record! &mdash;</strong> Over 7,000 voter registrations yesterday! </strong> <a href="http://voteforbernie.org/register-to-vote/" data-track="RegToVote,CTA">Get Registered Now</a> ?>
-            <strong>Help needed</strong> March 15th could make or break Bernie's campaign. <a href="http://voteforbernie.org/GOTV/" data-track="CTA,GOTV_MakeOrBreak">Get Out The Vote</a>
+            Over 135,000 voter registrations since February! #GOTR </strong> <a href="http://voteforbernie.org/register-to-vote/" data-track="RegToVote,CTA">Get Registered Now</a>
+            <?php // <strong>Help needed</strong> March 15th could make or break Bernie's campaign. <a href="http://voteforbernie.org/GOTV/" data-track="CTA,GOTV_MakeOrBreak">Get Out The Vote</a> ?>
             <div class="fb-like vfb-like" data-href="http://voteforbernie.org/" data-layout="button_count" data-action="like" data-show-faces="true" data-share="false"></div>
           </div>
           <div id="inner-header" class="branding wrap">
