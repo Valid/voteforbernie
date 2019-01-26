503036a4c09d0969f263e0fbef1616fcdc67ead5
Jon Hughes <github@validatorian.com>
2019-01-26 00:10:07 -0800

    Add AdSense info

--- header.php
+++ header.php
@@ -104,6 +104,13 @@
     return t;
     }(document, "script", "twitter-wjs"));</script>
   <?php // end analytics ?>
+  <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+  <script>
+    (adsbygoogle = window.adsbygoogle || []).push({
+      google_ad_client: "ca-pub-3543510024089552",
+      enable_page_level_ads: true
+    });
+  </script>
   </head>
 
   <body <?php body_class(); ?> itemscope itemtype="http://schema.org/WebPage">
