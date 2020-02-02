9e4756f12dc6c6fbdd723be71d34c315bcc4de14
Jon Hughes <jon@jonhughes.io>
2020-02-01 17:13:22 -0800

    plural

--- stickers.php
+++ stickers.php
@@ -39,7 +39,7 @@
         <img src="https://voteforbernie.org/wp-content/themes/voteforbernie/assets/images/vfb-stickers.jpg"
           alt="VoteForBernie.org Stickers" />
         <p>Be sure to select the option to share your address with us when making your donation so we know where to mail
-          your sticker.</p>
+          your stickers.</p>
         <p>Donate to VoteForBernie.org here:</p>
         <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
           <input type="hidden" name="cmd" value="_donations" />
